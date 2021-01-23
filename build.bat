9969a263c9541be5508a277dbaf02f94754a0301
Allen Webster <yoyo4thdimention@gmail.com>
2021-01-22 19:06:06 -0800

    [base] helper macros

--- build.bat
+++ build.bat
@@ -1,8 +1,10 @@
 @echo off
 
-set opts=-FC -GR- -EHa- -nologo -Zi -Fetest
-set clang_opts=-o test.exe
+set opts=-DENABLE_ASSERT=1
+set cl_opts=-FC -GR- -EHa- -nologo -Zi -Fetest %opts%
+set clang_opts=-o test.exe %opts%
 set code=%cd%
 pushd build
-clang %clang_opts% %code%\main.cpp
+cl %cl_opts% %code%\main.cpp
+rem clang %clang_opts% %code%\main.cpp
 popd
